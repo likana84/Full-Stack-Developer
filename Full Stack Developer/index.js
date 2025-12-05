c493563720e5c9196dc801b767a72345d7ba6ae5
walter <musunguwalter57@gmail.com>
2025-12-05 14:51:37 +0300

    modified saved function

--- Full Stack Developer/index.js
+++ Full Stack Developer/index.js
@@ -1,5 +1,9 @@
 //document.getElementById("number-of-people").innerHTML = "100";
 
+let countEl = document.getElementById("number-of-people");
+let saveEl = document.getElementById("save-el");
+
+//console.log("Jesus")
 let count = 0;
 console.log(count);
 // initialize the count as 0
@@ -7,13 +11,19 @@ console.log(count);
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
+
 function increment() {
     //console.log("The button was clicked");
-    count = count +1;
-    document.getElementById("number-of-people").innerText= count;
+    count += 1;
+    countEl.innerText= count;
     //console.log(count);
 }
 function save() {
-    console.log(count);
+    //create a variable that contains both the count and the dash separator 
+    let countStr = count +  "-";
+    //Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr;
+
+   //console.log(count);
 }
 save()
